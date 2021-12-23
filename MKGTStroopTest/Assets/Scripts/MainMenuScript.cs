e0add12065888782abdd75722815387125eb80b0
ashrenshugar <51221151+ashrenshugar@users.noreply.github.com>
2021-12-23 11:56:24 +1030

    Main menu
    Completion of main menu (basic)

---
+++ MKGTStroopTest/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+
+    public void TestScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void TrialScene()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        //Debug.Log("Quitting");
+    }
+
+}
